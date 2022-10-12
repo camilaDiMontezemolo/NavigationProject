5eb88b4579b053f5a09b545f3379b67dcf51fcec
tomasagata <tomasagata00@gmail.com>
2022-10-12 19:40:07 -0300

    Added Drawer Functionality (WIP)

--- App.js
+++ App.js
@@ -2,12 +2,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Screen1 from './src/screens/screen1/screen1.js';
 import Screen2 from './src/screens/screen2/screen2.js';
+import Drawer1 from './src/screens/drawer1/drawer1.js';
+import Drawer2 from './src/screens/drawer2/drawer2.js';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const App = () => {
+const RegularNavigationApp = () => {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
@@ -18,4 +23,15 @@ const App = () => {
 	);
 };
 
-export default App;
+const DrawerNavigationApp = () => {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="drawer1">
+                <Drawer.Screen name="drawer1" component={Drawer1} />
+                <Drawer.Screen name="drawer2" component={Drawer2} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default RegularNavigationApp;
